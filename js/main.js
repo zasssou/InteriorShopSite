736d13eb6951731f97cc41429162afc6e8d01f6e
zasssou <zassou.tnk@gmail.com>
2022-04-30 23:58:25 +0900

    improve header part.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 $(function() {
 
   $('.c-hamburger-menu').on('click', () => {
-    if ($('#header').hasClass('open-menu')) {
-      $('#header').removeClass('open-menu');
-    } else {
-      $('#header').addClass('open-menu');
-    }
+    $('#header').toggleClass('open-menu');
   });
 
   $('.c-overlay').on('click', () => {
-    if ($('#header').hasClass('open-menu')) {
-      $('#header').removeClass('open-menu');
-    }
+    $('#header').toggleClass('open-menu');
   });
 
 });
